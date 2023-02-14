4d552fa00a903888673589ab6f3d5fc8b57b50fd
sho037 <103867061+sho037@users.noreply.github.com>
2023-02-15 03:15:25 +0900

    🚧 地点を設定する際のひな形を作成

--- kc3hack_h/src/Weather.js
+++ kc3hack_h/src/Weather.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 reportWebVitals();
 
 function Weather() {
+  // 地点の設定
+  const [locationName, setLocationName] = useState("地点が設定されていません");
+  function handleLocationSettingClick() {
+    alert("地点を設定します");
+    setLocationName("地点が設定されました");
+  }
+
   return (
     <div>
       <h1>天気画面</h1>
+
+      <div id="location">
+        <h2 id="location-name">{locationName}</h2>
+        <div id="location-setting">
+          <button id="location-setting-button" onClick={handleLocationSettingClick}>
+            ✐
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
