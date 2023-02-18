8796a649c14dde9c54ec87b97359a2552fb532b6
sho037 <103867061+sho037@users.noreply.github.com>
2023-02-19 00:12:30 +0900

    ✨ アップバーを作成

--- kc3hack_h/src/index.js
+++ kc3hack_h/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-
+import Header from './Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>
 );
