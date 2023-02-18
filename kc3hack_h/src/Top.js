67c7bd5ad8e5212bc7954e9665e3ed80c0318871
natsuki <nattunn.com1417@gmail.com>
2023-02-18 15:38:58 +0900

    Traffic pwa化

--- kc3hack_h/src/Top.js
+++ kc3hack_h/src/Top.js
@@ -2,18 +2,22 @@ import React from 'react';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Weather from './Weather';
+import { useNavigate } from "react-router-dom";
 
 
 reportWebVitals();
 
 
 function Top(){
+  const navigate = useNavigate()
     
       return(
           <>
           <div>
             トップ画面
             <Weather/>
+            <button onClick={() => {navigate('/traffic')
+      }}>遅延情報</button>
           </div>
           </>
         )
