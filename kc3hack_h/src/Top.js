c8528f59f91c8d2fa28bdb0b91e48eca92f6b5ca
sho037 <103867061+sho037@users.noreply.github.com>
2023-02-18 21:34:02 +0900

    🎨 フォーマット

--- kc3hack_h/src/Top.js
+++ kc3hack_h/src/Top.js
@@ -1,37 +1,46 @@
-import React from 'react';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import Weather from './Weather';
-import Schedule from './Schedule';
-import ScheduleTime from './ScheduleTime';
+import React from "react";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Weather from "./Weather";
+import Schedule from "./Schedule";
+import ScheduleTime from "./ScheduleTime";
 import { useNavigate } from "react-router-dom";
 
-const calendarID = '282696404qq@gmail.com';
-const APIkey = 'AIzaSyB211YGz4rfmjrJIcf_9r036GvUsVqegtM';
-
+const calendarID = "282696404qq@gmail.com";
+const APIkey = "AIzaSyB211YGz4rfmjrJIcf_9r036GvUsVqegtM";
 
 reportWebVitals();
 
-function Top(){
-  const navigate = useNavigate()
+function Top() {
+  const navigate = useNavigate();
 
-  return(
+  return (
     <>
-    <div>
-      トップ画面
-      
-      <Weather/>
-      <button className="trafficButton" onClick={() => {navigate('/traffic')
-}}>遅延情報</button>
-      <br/>
-      <button className="scheduleButton" onClick={() => {navigate('/schedule')
-}}>予定情報</button>
-      {/*<Schedule calendarID={calendarID} APIkey={APIkey}/>*/}
-      <ScheduleTime />
-    </div>
+      <div>
+        トップ画面
+        <Weather />
+        <button
+          className="trafficButton"
+          onClick={() => {
+            navigate("/traffic");
+          }}
+        >
+          遅延情報
+        </button>
+        <br />
+        <button
+          className="scheduleButton"
+          onClick={() => {
+            navigate("/schedule");
+          }}
+        >
+          予定情報
+        </button>
+        {/*<Schedule calendarID={calendarID} APIkey={APIkey}/>*/}
+        <ScheduleTime />
+      </div>
     </>
-  )
-
+  );
 }
 
-export default Top; 
+export default Top;
