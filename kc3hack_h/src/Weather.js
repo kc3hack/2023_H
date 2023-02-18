2d8470d179cfd28030eed3de3d56556f33bca152
sho037 <103867061+sho037@users.noreply.github.com>
2023-02-18 13:14:55 +0900

    🔥 不要なインポートを削除

--- kc3hack_h/src/Weather.js
+++ kc3hack_h/src/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import weatherCodes from "./weatherCodes.json";
